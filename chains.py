c241a5971eecc6d93c8e727e5d3ec59924b5eb2f
breadbread1984 <breadbread1984@163.com>
2024-11-01 09:59:27 +0800

    update

--- chains.py
+++ chains.py
@@ -3,7 +3,7 @@
 from prompts import label_tanl_template
 
 def label_tanl_chain(llm, tokenizer):
-  tanl_template = extract_tanl_template(tokenizer)
-  tanl_chain = extract_tanl_template | llm
+  tanl_template = label_tanl_template(tokenizer)
+  tanl_chain = tanl_template | llm
   return tanl_chain
 
